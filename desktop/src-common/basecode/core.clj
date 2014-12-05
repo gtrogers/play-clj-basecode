3b121deadc59a74a990c65743cdeca925b5775c2
Gareth <grogers@thoughtworks.com>
2014-12-05 19:52:09 +0000

    Exampe of loading textures and text wrapping

--- desktop/src-common/basecode/core.clj
+++ desktop/src-common/basecode/core.clj
@@ -1,6 +1,18 @@
 (ns basecode.core
   (:require [play-clj.core :refer :all]
-            [play-clj.ui :refer :all]))
+            [play-clj.ui :refer :all]
+            [play-clj.g2d :refer :all]
+            ))
+
+(defn- update-text [entities]
+  (map (fn [e] (if (:text e) (do (label! e :set-text "key pressed") (prn e) e) e)) entities))
+
+(defn- text-box! [text width]
+  (let [l (label text (color :white))]
+   (doto l 
+     (label! :set-wrap true)
+     (label! :set-width width)) 
+    ))
 
 (defscreen main-screen
   :on-show
@@ -8,17 +20,24 @@
     (update! screen :renderer (stage))
     [
      ; some text
-     (assoc (label "Hello world!" (color :white)) :x 200) 
+     (assoc (text-box! "This is some really long text becasue I want to see if word wrapping works" 100) :x 300 :y 200 :text true) 
      ; a simple shape
      (shape :filled :rect 100 100 50 50 :set-color (color :green)) 
+     ; a texture
+     (assoc (texture "test_square.png") :x 400 :y 100) 
      ])
   
   :on-render
   (fn [screen entities]
     (clear!)
-    (render! screen entities)))
+    (render! screen entities))
+
+  :on-key-down 
+  (fn [screen entities]
+    (when (key-pressed? :r) (update-text entities)))) 
 
 (defgame basecode
   :on-create
   (fn [this]
     (set-screen! this main-screen)))
+
